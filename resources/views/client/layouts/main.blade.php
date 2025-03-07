c8f757dfabde6400037934e60dffba3696268707
Asim <asimmahmood11110@gmail.com>
2025-03-07 12:24:55 +0500

    Initial commit

---
+++ resources/views/client/layouts/main.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+    @include('client.partials.header')
+    @include('client.partials.navbar')
+    <body>
+        @yield('page')
+    </body>
+    @include('client.partials.footer')
+</html>
