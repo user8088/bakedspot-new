8b5e022932d774bae873006a869e266310f8ae63
Asim <asimmahmood11110@gmail.com>
2025-03-15 14:05:37 +0500

    Footer created

--- resources/views/client/partials/footer.blade.php
+++ resources/views/client/partials/footer.blade.php
@@ -1,7 +1,71 @@
-<footer>
+<footer class="border-0 border-top">
+    <div class="container pt-3">
+        <div class="row p-3  text-center">
+            <div class="col-4 col-lg-3">
+                <h5 class="fw-bold">Company</h5>
+            </div>
+            <div class="col-2 col-lg-2">
+                Link 1
+            </div>
+            <div class="col-2 col-lg-2">
+                Link 2
+            </div>
+            <div class="col-2 col-lg-2">
+                Link 3
+            </div>
+            <div class="col-2 col-lg-2">
+                Link 4
+            </div>
+        </div>
+        <div class="row p-3  text-center">
+            <div class="col-4 col-lg-3">
+                <h5 class="fw-bold">Collab</h5>
+            </div>
+            <div class="col-2 col-lg-2">
+                Link 1
+            </div>
+            <div class="col-2 col-lg-2">
+                Link 2
+            </div>
+            <div class="col-2 col-lg-2">
+                Link 3
+            </div>
+            <div class="col-2 col-lg-2">
+                Link 4
+            </div>
+        </div>
+        <div class="row p-3 pb-5 text-center">
+            <div class="col-lg-12 ">
+                <img src="{{ asset('icons/instagram-icon.png') }}" class="pe-3" width="50px" alt="">
+                <img src="{{ asset('icons/facebook-icon.png') }}" class="pe-3" width="50px" alt="">
+                <img src="{{ asset('icons/whatsapp-icon.png') }}" class="pe-3" width="50px" alt="">
+                <img src="{{ asset('icons/instagram-icon.png') }}" class="pe-3" width="50px" alt="">
+                <img src="{{ asset('icons/facebook-icon.png') }}" class="pe-3" width="50px" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid text-center pb-5" style="background-color: #ffc0cb;">
+        <img src="{{ asset('images/logo.png') }}" class="footer-logo" alt="">
+        <p>2025 @ all rights reserved.</p>
+        <p>Privacy policy
+            |
+            Terms and Conditions
+            |
+            Cookie Preferences</p>
+    </div>
+
+
+
+
+
+
+
+    {{-- Scripts --}}
     <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.7/dist/gsap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.7/dist/ScrollTrigger.min.js"></script>
     <script src="https://unpkg.com/lenis@1.1.20/dist/lenis.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="{{asset('js/scripts.js')}}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+    <script src="{{ asset('js/scripts.js') }}"></script>
 </footer>
